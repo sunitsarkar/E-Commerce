f515100f4ad52fcb9d21b451b3980f6fbfcd5928
sunit sarkar <sunit6503@gmail.com>
2023-07-22 00:00:14 +0530

    stracture complete

---
+++ src/component/landingPage.tsx
@@ -0,0 +1,38 @@
+
+
+export default function LandingPage() {
+    return <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">E-commerce</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Category
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <label className="form-label">Example range</label>
+                                <input type="range" className="form-range" min="0" max="5" id="customRange2" />
+                            </li>
+                            <li><label className="form-label">Example range</label>
+                                <input type="range" className="form-range" min="0" max="5" id="customRange2" /></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form className="d-flex" role="search">
+                    <li className="nav-item">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                    </li>
+                </form>
+            </div>
+        </div>
+    </nav>
+}
---
+++ src/component/registration.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Register(){
+    <div>
+        
+    </div>
+}
